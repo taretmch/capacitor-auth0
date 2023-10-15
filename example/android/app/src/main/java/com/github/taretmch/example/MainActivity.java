e455c7cd3ca2f53957afe848dfdd8117b8c17bd7
taretmch <tomoki.mizogami@gmail.com>
2023-10-15 20:58:17 +0900

    Revert "refactor(example): Convert java to kotlin"
    This reverts commit 34acbc2193523b7a3f13ff295f5658b5b36d6714.

---
+++ example/android/app/src/main/java/com/github/taretmch/example/MainActivity.java
@@ -0,0 +1,5 @@
+package com.github.taretmch.example;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- example/android/app/src/main/java/com/github/taretmch/example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.github.taretmch.example
-
-import com.getcapacitor.BridgeActivity
-
-class MainActivity : BridgeActivity()
