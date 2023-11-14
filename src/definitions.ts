5764113a9860f64f024c0a1ac2bb414c39f51773
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2023-11-15 00:33:51 +0900

    refactor: Add comment to definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,16 +1,61 @@
+/**
+ * Capacitor Auth0 Plugin
+ */
 export interface CapacitorAuth0Plugin {
+
+  /**
+   * Configure the plugin with your Auth0 credentials.
+   * @param options  CapacitorAuth0Conf
+   * @returns Promise<void>
+   */
   configure(options: CapacitorAuth0Conf): Promise<void>;
+
+  /**
+   * Web Auth: Login with Auth0.
+   * @returns Promise<Auth0User>
+   */
   login(): Promise<Auth0User>;
+
+  /**
+   * Web Auth: Logout from Auth0.
+   * @returns Promise<void>
+   */
   logout(): Promise<void>;
 }
 
+/**
+ * Configuration options for the plugin.
+ */
 export interface CapacitorAuth0Conf {
+
+  /**
+   * Your Auth0 domain.
+   */
   domain: string;
+
+  /**
+   * Your Auth0 client ID.
+   */
   clientId: string;
 }
 
+/**
+ * Auth0 user profile.
+ */
 export interface Auth0User {
+
+  /**
+   * User ID.
+   */
   id: string;
+
+  /**
+   * User name.
+   */
   name: string;
+
+  /**
+   * User email.
+   */
   email: string;
 }
