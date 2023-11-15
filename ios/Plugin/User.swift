25e4824fcd47b039d19c09ad9e401d05c00b2616
taretmch <tomoki.mizogami@gmail.com>
2023-11-15 14:03:15 +0900

    refactor(ios): Refactor guard block

--- ios/Plugin/User.swift
+++ ios/Plugin/User.swift
@@ -17,11 +17,7 @@ struct User {
 
 extension User {
     init?(from idToken: String) {
-        guard let jwt = try? decode(jwt: idToken) else {
-            return nil
-        }
-
-        guard
+        guard let jwt = try? decode(jwt: idToken),
               let id = jwt.subject,
               let name = jwt["name"].string else {
             return nil
